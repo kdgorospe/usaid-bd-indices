e098dc3fdfe800b9e7d04f5bbd8a1220bd72b2a8
Kelvin Gorospe <kdgorospe@gmail.com>
2022-05-31 17:39:39 -0400

    start usaid bd indices project

---
+++ marine-indices.R
@@ -0,0 +1,3 @@
+# First pass marine biodiversity indices
+# Resources: https://www.marinedatascience.co/data/
+# .RMD for using OBIS data on marine biodiversity indicators: https://iobis.github.io/notebook-diversity-indicators/
