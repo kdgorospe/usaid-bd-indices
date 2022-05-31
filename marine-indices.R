e502b425f1ea2da2b95467b9ec19bd1cb46d9f79
Kelvin Gorospe <kdgorospe@gmail.com>
2022-05-31 17:48:34 -0400

    read in data

--- marine-indices.R
+++ marine-indices.R
@@ -1,3 +1,8 @@
 # First pass marine biodiversity indices
 # Resources: https://www.marinedatascience.co/data/
 # .RMD for using OBIS data on marine biodiversity indicators: https://iobis.github.io/notebook-diversity-indicators/
+
+# Data from Selig et al 2013 PLOS One: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0060284#s4
+
+s6 <- read.csv("/Users/kgorospe/Documents/Git/usaid-bd-indices/Data/Selig-et-al-2013_PLOS-One_Table-S6.csv")
+s7 <- read.csv("/Users/kgorospe/Documents/Git/usaid-bd-indices/Data/Selig-et-al-2013_PLOS-One_Table-S7.csv")
