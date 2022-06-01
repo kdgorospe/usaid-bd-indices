2e662f786c4ede38a95c2d21126098394878ecaa
Kelvin Gorospe <kdgorospe@gmail.com>
2022-06-01 09:23:12 -0400

    add notes

--- marine-indices.R
+++ marine-indices.R
@@ -6,3 +6,5 @@
 
 s6 <- read.csv("/Users/kgorospe/Documents/Git/usaid-bd-indices/Data/Selig-et-al-2013_PLOS-One_Table-S6.csv")
 s7 <- read.csv("/Users/kgorospe/Documents/Git/usaid-bd-indices/Data/Selig-et-al-2013_PLOS-One_Table-S7.csv")
+
+# LEFT OFF: See indices used by Blue Ventures analysis (shared by Nirmal): https://docs.google.com/document/d/1jULcqx9KLnCvV5TChK_Al_RwND2dkjAYARUTWKoAkX4/edit#heading=h.eh8au4e07o4b
