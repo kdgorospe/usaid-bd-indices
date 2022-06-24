dba3ea8be449ac057c88ccd37a46f2914a4de699
Kelvin Gorospe <kdgorospe@gmail.com>
2022-06-24 14:34:37 -0400

    add Ocean Health index data

--- marine-indices.R
+++ marine-indices.R
@@ -10,7 +10,68 @@ library(countrycode)
 # .RMD for using OBIS data on marine biodiversity indicators: https://iobis.github.io/notebook-diversity-indicators/
 
 ####################################################################################
-# Data from Selig et al 2013 PLOS One: Assessing Global Marine Biodiversity Status within a Coupled Socio-Ecological Perspective
+# Ocean Health Index data: https://oceanhealthindex.org/global-scores/
+
+bd_scores <- read.csv("/Users/kgorospe/Documents/Git/usaid-bd-indices/Data/scores.csv") %>%
+  mutate(iso3a = countrycode(bd_scores$region_name, origin = "country.name", destination = "iso3c"))
+  
+####################################################################################
+# Check bd_scores for what needs to be cleaned
+# These are the region_names that need did not match an ISO3A
+tmp_modify_region_name <- bd_scores %>%
+  filter(is.na(iso3a)) %>%
+  distinct(region_name)
+# Result: FIX Micronesia below
+
+# Find region_names that resulted in same iso3a
+tmp_remove <- bd_scores %>%
+  select(iso3a, scenario, goal, long_goal, dimension, value) %>% 
+  arrange(iso3a, scenario, goal, long_goal, dimension, value) %>%
+  group_by(iso3a, scenario, goal, long_goal, dimension, value) %>%
+  filter(n()>1) %>%
+  ungroup() %>%
+  pull(iso3a) %>%
+  unique()
+
+# List the region_names that resulted in same iso3a
+bd_scores %>% 
+  filter(!is.na(iso3a) & iso3a %in% tmp_remove) %>%
+  select(region_name, iso3a) %>%
+  distinct()
+# RESULT: Remove Bassas da India for now (French territory, should not match to IND)
+# Then average across KIR indices
+# Also standardize region_name for Kiribati (Line Islands, Phoenix Islands, and Gilbert Islands were all matched to KIR)
+
+
+####################################################################################
+# Clean biodiversity region_names based on "tmp" object info above
+bd_scores_clean <- bd_scores %>%
+  # Edit "Micronesia" to be FSM
+  mutate(region_name = if_else(region_name == "Micronesia", true = "Federated States of Micronesia", false = region_name)) %>%
+  # Edit (Line Islands, Phoenix Islands, and Gilbert Islands) to be Kiribati
+  mutate(region_name = if_else(str_detect(region_name, pattern = "Kiribati"), true = "Kiribati", false = region_name)) %>%
+  # Remove Bassas da India
+  filter(region_name != "Bassas da India") %>%
+  # After cleaning region_name, match to iso3a
+  mutate(iso3a = countrycode(region_name, origin = "country.name", destination = "iso3c")) %>%
+  # Group by and average scores - needed for KIRIBATI (multiple values since Line Islands, Phoenix Islands, and Gilbert Islands were all modified to match to KIR)
+  group_by(iso3a, region_name, scenario, goal, long_goal, dimension) %>%
+  summarise(value = mean(value)) %>%
+  ungroup()
+
+# CHECKS:
+# bd_scores_clean %>%
+#   filter(iso3a == "KIR")
+# 
+# bd_scores_clean %>%
+#   filter(iso3a == "FSM")
+  
+# LEFT OFF HERE: Check metadata to decide which indices match Nirmal's framework and begin ordering and extracting these values
+
+
+
+####################################################################################
+# OLD Code: Data from Selig et al 2013 PLOS One: Assessing Global Marine Biodiversity Status within a Coupled Socio-Ecological Perspective
 # https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0060284#s4
 
 # Species and habitat scores by country
